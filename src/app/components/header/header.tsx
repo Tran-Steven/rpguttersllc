e14b817c1d2d37a61c8be08233bb09ed298ec410
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-08-19 16:55:06 -0400

    added header

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "@/assets/logo";
+function header() {
+  <div>
+    <img src={logo} alt="company logo"></img>
+  </div>;
+}
+
+export default header;
---
+++ src/app/components/main/main.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+function main() {
+  <div></div>;
+}
+
+export default main;
+<div></div>;
