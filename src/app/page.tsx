3b555140d268aa38cb9bd2a65755d3f000dcd522
Steven Tran <steven29160@gmail.com>
2024-08-11 13:15:18 -0400

    fix paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import styles from "page.module.css";
-import List from "./components/Listheader";
-import Contact from "./components/Contactheader";
-import Main from "./components/Main";
+import List from "components/listheader";
+import Contact from "components/contactheader";
+import Main from "components/main";
 import Footer from "./components/Footer";
 export default function Home() {
   return (
